895713cd8d77556d56b5b4e6e2dc61272ce80f6f
tPoH3 <90758356+tPoH3@users.noreply.github.com>
2023-06-17 16:59:31 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package sp.senai.br.operacoesmatematicas;
+
+public class Main {
+    
+    public static void main(String args[]){   
+       
+        Operacoes objetos = new Operacoes();
+        
+        double soma = objetos.adicao(10, 10);
+                
+                
+        System.out.println(soma);
+    	System.exit(0);
+		
+    }
+}
---
+++ Operacoes.java
@@ -0,0 +1,7 @@
+package sp.senai.br.operacoesmatematicas;
+
+public class Operacoes {
+    public static double adicao(double valor1, double valor2){
+        return valor1 + valor2;
+    }
+}
